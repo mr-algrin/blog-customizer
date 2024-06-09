4131501cf730f0179160be6b8a108e6c7440af9c
Alex Grin <mr.algrin@gmail.com>
2024-06-09 19:56:45 +0300

    implemented article params form

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
@@ -5,15 +6,25 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export interface ArrowButtonProps {
+	onClick: OnClick;
+	isOpen: boolean;
+}
+
+export const ArrowButton = (props: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, props.isOpen && styles.container_open)}
+			onClick={props.onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, props.isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,90 @@
+import { FormEvent, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from 'components/select';
+import { Separator } from 'components/separator';
+import { RadioGroup } from 'components/radio-group';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	fontFamilyOptions,
+	fontColors,
+	fontSizeOptions,
+	backgroundColors,
+	defaultArticleState,
+	contentWidthArr,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [state, setState] = useState<ArticleStateType>({
+		...defaultArticleState,
+	});
+
+	const onChange =
+		<Key extends keyof ArticleStateType>(key: Key) =>
+		(value: ArticleStateType[Key]) => {
+			setState((prev) => ({ ...prev, [key]: value }));
+		};
+
+	const onClick = () => setIsOpen((prev) => !prev);
+
+	const onApply = (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+	};
+
+	const onReset = () => setState({ ...defaultArticleState });
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div>
+			<ArrowButton onClick={onClick} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form className={styles.form} onSubmit={onApply} onReset={onReset}>
+					<div className={styles.content}>
+						<span className={styles.title}>Задайте параметры</span>
+						<Select
+							title={'Шрифт'}
+							selected={state.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={onChange('fontFamilyOption')}
+						/>
+						<RadioGroup
+							title={'Размер шрифта'}
+							name={'fontSize'}
+							options={fontSizeOptions}
+							selected={state.fontSizeOption}
+							onChange={onChange('fontSizeOption')}
+						/>
+						<Select
+							title={'Цвет шрифта'}
+							selected={state.fontColor}
+							options={fontColors}
+							onChange={onChange('fontColor')}
+						/>
+						<Separator />
+						<Select
+							title={'Цвет фона'}
+							selected={state.backgroundColor}
+							options={backgroundColors}
+							onChange={onChange('backgroundColor')}
+						/>
+						<Select
+							title={'Ширина контента'}
+							selected={state.contentWidth}
+							options={contentWidthArr}
+							onChange={onChange('contentWidth')}
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
